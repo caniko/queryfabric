af829facfc0666196eddee9a07abc887d4b10d45
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-06-16 22:55:34 +0200

    chore: update pre-commit nix config

--- nix/pre-commit.nix
+++ nix/pre-commit.nix
@@ -2,8 +2,7 @@
   pkgs,
   treefmtWrapper,
   rustToolchain ? null,
-}:
-{
+}: {
   treefmt = {
     enable = true;
     name = "treefmt";
@@ -31,19 +30,24 @@
     enable = true;
     name = "cargo check MSRV";
     entry = "${pkgs.rust-bin.stable."1.88.0".default}/bin/cargo check --workspace --all-features";
-    extraPackages = [ pkgs.rust-bin.stable."1.88.0".default ];
+    extraPackages = [pkgs.rust-bin.stable."1.88.0".default];
     pass_filenames = false;
-    stages = [
-      "pre-push"
-      "manual"
-    ];
+    stages = ["pre-push" "manual"];
   };
 
   cargo-audit = {
     enable = true;
     name = "cargo audit";
     entry = "cargo audit";
-    extraPackages = pkgs.lib.optional (rustToolchain != null) rustToolchain ++ [ pkgs.cargo-audit ];
+    extraPackages = pkgs.lib.optional (rustToolchain != null) rustToolchain ++ [pkgs.cargo-audit];
+    pass_filenames = false;
+  };
+
+  cargo-deny = {
+    enable = true;
+    name = "cargo deny";
+    entry = "cargo deny check bans licenses sources";
+    extraPackages = pkgs.lib.optional (rustToolchain != null) rustToolchain ++ [pkgs.cargo-deny];
     pass_filenames = false;
   };
 
@@ -51,8 +55,8 @@
     enable = true;
     name = "nix flake check";
     entry = "nix --extra-experimental-features 'nix-command flakes' flake check --cores 0 --max-jobs auto --no-update-lock-file";
-    extraPackages = [ pkgs.nix ];
+    extraPackages = [pkgs.nix];
     pass_filenames = false;
-    stages = [ "manual" ];
+    stages = ["manual"];
   };
 }
